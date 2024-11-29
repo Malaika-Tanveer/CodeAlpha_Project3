1a4596ee65fd070e22c3eeabbac865a087365800
Malaika Tanveer <malaikatanveer061@gmail.com>
2024-11-30 00:25:30 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// JavaScript for calculator functionality
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let current = document.getElementById('result').value;
+    document.getElementById('result').value = current.slice(0, -1);
+}
+
+function appendToScreen(value) {
+    document.getElementById('result').value += value;
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
